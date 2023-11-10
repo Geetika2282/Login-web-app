bc710b469e7d5f5bef93bd8e8ecfefdffee27eac
Geetika <geet1kanwar@gmail.com>
2023-11-10 19:48:18 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("submit");
+const clearButton = document.getElementById("clear")
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
@@ -9,8 +10,8 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     if (username === "user" && password === "123") {
-        alert("You have successfully logged in.");
-        // location.reload();
+        alert("You have successfully logged in!");
+        location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
     }
